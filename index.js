d81462d5e355e6aed04c7b562f191c8961afad20
James Boehme <jamesrbdev@gmail.com>
2022-01-30 14:02:31 -0600

    Add express JSON middleware

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const PORT = 8080;
 
 app.listen(PORT, () => console.log(`API initialized on PORT ${PORT}`));
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
 	res.status(200).send({
 		test: "success"
